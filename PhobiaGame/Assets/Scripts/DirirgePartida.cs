fb922d403c5256624ab7b82481cff5ecb6143570
Miguel Ángel Buigues <al376426@uji.es>
2021-02-14 14:26:47 +0100

    Iconos 3D pendientes

---
+++ PhobiaGame/Assets/Scripts/DirirgePartida.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirirgePartida : MonoBehaviour
+{
+    public Camera camaraJuego;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Camera.main = camaraJuego;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
